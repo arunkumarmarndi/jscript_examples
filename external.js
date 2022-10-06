2c5190e65dfb026e923b5a00ea4e7c8a4cdb4542
Arun Kumar Marndi <arunkumarmarndi1994@gmail.com>
2022-10-06 12:45:54 +1100

    Modified playAround() to check if the player won or lost

--- external.js
+++ external.js
@@ -1,6 +1,15 @@
-const array = ["Rock","Paper","Scissors"];
+const possible_choices = ["Rock","Paper","Scissors"];
+function getComputerChoice(){
+    return possible_choices[Math.floor(Math.random()*3)];
+}
 function playAround(playerSelection,computerSelection){
-    //your code here!
+    if (playerSelection.toUpperCase() == computerSelection.toUpperCase()){
+        return "It is a draw!";
+    } else if(computerSelection.toUpperCase() == "PAPER"){
+        return "You lose! Paper beats Rock";
+    } else{
+        return "You win! Rock beats Scissors";
+    }
 }
 
 const playerSelection = "rock";
